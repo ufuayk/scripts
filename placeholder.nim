fddb2dc030fd195d6585075b88d4323522b9dff3
Ufuk <132388044+ufuayk@users.noreply.github.com>
2026-09-12 20:46:42 +0300

    it must be nim!

--- placeholder.nim
+++ placeholder.nim
@@ -1,8 +1,13 @@
 # placeholder
 
-var a: int
-var b: int
-var c: int
+type
+  Dummy1 = object
+    val1: int
+    val2: string
+  Dummy2 = object
+    val3: float
+  Dummy3 = enum
+    optA, optB, optC
 
 var x1: int
 var x2: int
@@ -43,6 +48,46 @@ var x36: int
 var x37: int
 var x38: int
 var x39: int
+var x40: int
+var x41: int
+var x42: int
+var x43: int
+var x44: int
+var x45: int
+var x46: int
+var x47: int
+var x48: int
+var x49: int
+var x50: int
+var x51: int
+var x52: int
+var x53: int
+var x54: int
+var x55: int
+var x56: int
+var x57: int
+var x58: int
+var x59: int
+var x60: int
+var x61: int
+var x62: int
+var x63: int
+var x64: int
+var x65: int
+var x66: int
+var x67: int
+var x68: int
+var x69: int
+var x70: int
+var x71: int
+var x72: int
+var x73: int
+var x74: int
+var x75: int
+var x76: int
+var x77: int
+var x78: int
+var x79: int
 
 proc noop1() =
   discard
@@ -131,8 +176,363 @@ proc noop28() =
 proc noop29() =
   discard
 
+proc noop30() =
+  discard
+
+proc noop31() =
+  discard
+
+proc noop32() =
+  discard
+
+proc noop33() =
+  discard
+
+proc noop34() =
+  discard
+
+proc noop35() =
+  discard
+
+proc noop36() =
+  discard
+
+proc noop37() =
+  discard
+
+proc noop38() =
+  discard
+
+proc noop39() =
+  discard
+
+proc noop40() =
+  discard
+
+proc noop41() =
+  discard
+
+proc noop42() =
+  discard
+
+proc noop43() =
+  discard
+
+proc noop44() =
+  discard
+
+proc noop45() =
+  discard
+
+proc noop46() =
+  discard
+
+proc noop47() =
+  discard
+
+proc noop48() =
+  discard
+
+proc noop49() =
+  discard
+
+proc noop50() =
+  discard
+
+proc noop51() =
+  discard
+
+proc noop52() =
+  discard
+
+proc noop53() =
+  discard
+
+proc noop54() =
+  discard
+
+proc noop55() =
+  discard
+
+proc noop56() =
+  discard
+
+proc noop57() =
+  discard
+
+proc noop58() =
+  discard
+
+proc noop59() =
+  discard
+
+proc empty1(a: int, b: int): int =
+  discard
+
+proc empty2(a: int, b: int): int =
+  discard
+
+proc empty3(a: int, b: int): int =
+  discard
+
+proc empty4(a: int, b: int): int =
+  discard
+
+proc empty5(a: int, b: int): int =
+  discard
+
+proc empty6(a: int, b: int): int =
+  discard
+
+proc empty7(a: int, b: int): int =
+  discard
+
+proc empty8(a: int, b: int): int =
+  discard
+
+proc empty9(a: int, b: int): int =
+  discard
+
+proc empty10(a: int, b: int): int =
+  discard
+
+proc empty11(a: int, b: int): int =
+  discard
+
+proc empty12(a: int, b: int): int =
+  discard
+
+proc empty13(a: int, b: int): int =
+  discard
+
+proc empty14(a: int, b: int): int =
+  discard
+
+proc empty15(a: int, b: int): int =
+  discard
+
+proc empty16(a: int, b: int): int =
+  discard
+
+proc empty17(a: int, b: int): int =
+  discard
+
+proc empty18(a: int, b: int): int =
+  discard
+
+proc empty19(a: int, b: int): int =
+  discard
+
+proc empty20(a: int, b: int): int =
+  discard
+
+proc empty21(a: int, b: int): int =
+  discard
+
+proc empty22(a: int, b: int): int =
+  discard
+
+proc empty23(a: int, b: int): int =
+  discard
+
+proc empty24(a: int, b: int): int =
+  discard
+
+proc empty25(a: int, b: int): int =
+  discard
+
+proc empty26(a: int, b: int): int =
+  discard
+
+proc empty27(a: int, b: int): int =
+  discard
+
+proc empty28(a: int, b: int): int =
+  discard
+
+proc empty29(a: int, b: int): int =
+  discard
+
 when isMainModule:
   discard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
